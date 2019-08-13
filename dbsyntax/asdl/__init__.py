8682f66c14d6ede097d68090e9f07d3933f702ac
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2019-08-14 00:50:22 +0700

    Remove old unused files & Add lark parser

--- dbsyntax/asdl/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Abstract Syntax Description Language (ASDL) package in python.
-
-Description of ASDL is described in [ASDL97] paper, which is located at:
-https://www.cs.princeton.edu/~appel/papers/asdl97.pdf
-"""
--- dbsyntax/collections/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Language collection package containing abstract syntax tress and
-parsers for various languages.
-
-To test the ASDL source file, run the following command from the
-project root directory::
-
-    $ python -m dbsyntax.asdl.parser dbsyntax/collections/<language>.asdl
-    
-"""
