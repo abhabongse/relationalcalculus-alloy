11dcf97b89c204dca8571668accfc1a58c440bdd
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2019-08-20 09:51:48 +0700

    Add variable shadowing check

--- relcal/syntax/drc.py
+++ relcal/syntax/drc.py
@@ -1,11 +1,12 @@
 """
 Lark parser and AST definition for Domain Relational Calculus.
 """
-from typing import Dict, NamedTuple, Tuple
+import pprint
+from typing import Dict, List, NamedTuple, Tuple, Union
 
-from lark import Lark, Tree, Transformer, v_args
+from lark import Lark, Token, Tree, Transformer, Visitor, v_args
 
-drc_parser = Lark("""
+drc_parser = Lark('''
 start: table_defs query
 table_defs: (table ";")*
 table: TABLE_NAME "(" fields? ")"
@@ -21,8 +22,8 @@ query: "{" fields ":" iff_test "}"
 ?atom_test: "(" iff_test ")"
           | table 
           | FIELD_NAME COMP_OP FIELD_NAME  -> compare_op
-          | _FOR_ALL_OP "[" FIELD_NAME "]" "(" or_test ")"  -> for_all
-          | _THERE_EXISTS_OP "[" FIELD_NAME "]" "(" or_test ")" -> there_exists
+          | _FOR_ALL_OP "[" FIELD_NAME "]" "(" iff_test ")"  -> for_all
+          | _THERE_EXISTS_OP "[" FIELD_NAME "]" "(" iff_test ")" -> there_exists
          
 TABLE_NAME: /[A-Z][A-Za-z0-9_]*/
 FIELD_NAME: /[a-z][A-Za-z0-9_]*/
@@ -40,7 +41,7 @@ COMP_OP: "==" | "!=" | ">=" | ">" | "<=" | "<"
 
 %import common.WS
 %ignore WS
-""", parser="lalr", debug=True)
+''', parser="lalr", debug=True)
 
 ####################
 # Type definitions #
@@ -64,50 +65,97 @@ class DRCExtractedData(NamedTuple):
     query: Query
 
 
-#########################
-# Lark tree transformer #
-#########################
+class Operation(NamedTuple):
+    op_name: str
+    args: Tuple[Union['Operation', str], ...]
 
-class DRCTreeTransformer(Transformer):
+
+#######################################
+# Lark tree visitors and transformers #
+#######################################
+
+# TODO: Lark tree visitors and transformers are not flexible enough
+#       Just transform them from scratch.
+
+class ValidateShadowingVisitor(Visitor):
     """
-    Parse tree transformer for DRC syntax grammar.
+    Parsed tree visitor to validate that no variable shadowing exists.
+    """
+
+    @staticmethod
+    def validate(variable: Token, tree: Tree):
+        for node in tree.iter_subtrees():
+            if node.data not in ('for_all', 'there_exists'):
+                continue
+            token = node.children[0]
+            if token == variable:
+                raise SyntaxError(f"variable {str(token)!r} overshadowed "
+                                  f"at line {token.line} column {token.column}")
+
+    def query(self, tree: Tree):
+        fields: Tree
+        predicate: Tree
+        fields, predicate = tree.children
+        for variable in fields.children:
+            self.validate(variable, predicate)
+
+    def for_all(self, tree: Tree) -> Tree:
+        bound_variable: Token
+        predicate: Tree
+        bound_variable, predicate = tree.children
+        self.validate(bound_variable, predicate)
+
+    def there_exists(self, tree: Tree) -> Tree:
+        bound_variable: Token
+        predicate: Tree
+        bound_variable, predicate = tree.children
+        self.validate(bound_variable, predicate)
+
+
+class DRCExtractionTransformer(Transformer):
+    """
+    Parsed tree transformer to obtain all table definitions
+    from the source data in DRC syntax grammar.
     """
 
     @v_args(inline=True)
-    def start(self, table_defs, query):
+    def start(self, table_defs: Dict[str, Fields], query: Query) -> DRCExtractedData:
         return DRCExtractedData(table_defs, query)
 
-    def table_defs(self, tables):
+    def table_defs(self, tables: List[Table]) -> Dict[str, Fields]:
         return dict(tables)
 
     @v_args(inline=True)
-    def table(self, table_name, fields):
+    def table(self, table_name: Token, fields: Fields) -> Table:
         return Table(str(table_name), fields)
 
-    def fields(self, field_names):
-        field_names = tuple(str(name) for name in field_names)
-        found = set()
-        for name in field_names:
-            if name in found:
-                raise ValueError(f"duplicated field name: {name}")
-            found.add(name)
-        return field_names
+    def fields(self, field_names: List[Token]) -> Fields:
+        return tuple(str(name) for name in field_names)
 
     @v_args(inline=True)
-    def query(self, tuple_vars, predicate):
+    def query(self, tuple_vars: Fields, predicate: Tree) -> Query:
         return Query(tuple_vars, predicate)
 
 
-drc_tree_transformer = DRCTreeTransformer()
+validate_shadowing_visitor = ValidateShadowingVisitor()
+drc_extraction_transformer = DRCExtractionTransformer()
 
 
 ##################
 # Main functions #
 ##################
 
-def extract_tree_data(tree: Tree) -> DRCExtractedData:
+def extract_tree_data(tree: Tree):
     """
     Obtain the table definitions from DRC syntax tree
     as a dictionary mapping from table names to tuple of field names.
     """
-    return drc_tree_transformer.transform(tree)
+    validate_shadowing_visitor.visit(tree)
+
+    table_defs: Dict[str, Fields]
+    query: Query
+    table_defs, query = drc_extraction_transformer.transform(tree)
+
+    pprint.pprint(table_defs)
+    pprint.pprint(query.tuple_vars)
+    print(query.predicate.pretty())
